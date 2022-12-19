760e6b14ca72ea32f2292a5a085c37b055af123a
Julien Ribeaucourt <55736383+RibJulien@users.noreply.github.com>
2022-12-19 10:34:28 +0100

    📄 Add index/sort/show Establishment

--- src/Controller/EstablishmentController.php
+++ src/Controller/EstablishmentController.php
@@ -19,4 +19,34 @@ class EstablishmentController extends AbstractController
             'establishments' => $establishmentList,
         ]);
     }
+
+    #[Route('/ehpad', name:'app_establishment_ehpad')]
+    public function ehpadList(ManagerRegistry $doctrine): Response 
+    {
+        $ehpadList = $doctrine->getRepository(Establishment::class)->findBy(['type' => 'EHPAD']);
+
+        return $this->render('establishment/ehpadList.html.twig', [
+            'establishments' => $ehpadList,
+        ]);
+    }
+
+    #[Route('/rss', name:'app_establishment_rss')]
+    public function rssList(ManagerRegistry $doctrine): Response
+    {
+        $rssList = $doctrine->getRepository(Establishment::class)->findBy(['type' => 'Résidence Services Seniors']);
+
+        return $this->render('establishment/rssList.html.twig', [
+            'establishments' => $rssList,
+        ]);
+    }
+
+    #[Route('/etablissement/{id}', name: 'app_establishment_show')]
+    public function establishmentShow(ManagerRegistry $doctrine, int $id): Response
+    {
+        $establishment = $doctrine->getRepository(Establishment::class)->find($id);
+        
+        return $this->render('establishment/show.html.twig', [
+            'establishment' => $establishment,
+        ]);
+    }
 }
